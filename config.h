c02ee1357fa4cfc94f67f85143b2865003b65f9e
Karl Ragnar Giese <karl@giese.no>
2021-01-14 14:33:27 +0100

    initial commit

--- config.h
+++ config.h
@@ -13,11 +13,11 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_accent[]      = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_accent,  col_accent  },
 };
 
 /* tagging */
@@ -28,9 +28,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class		instance	title	tags mask	is floating		monitor */
+	{ "Gimp",		NULL,		NULL,	0,			1,				-1 },
+	{ "Firefox",	NULL,		NULL,	1 << 8,		0,				-1 },
+	{ "QjackCtl",	NULL,		NULL,	1 << 7, 	0,				-1 },
+	{ "lsp-plugins",NULL,		NULL,	1 << 7,		0,				-1 },
 };
 
 /* layout(s) */
