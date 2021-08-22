cf6156500f9261e3f40d2829f5f12bb62154d446
karl.giese <karl@giese.no>
2021-08-22 16:28:23 +0200

    Last commit before flexpatch

--- config.h
+++ config.h
@@ -7,13 +7,13 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
-static const char *fonts[]          = { "firacode:size=10" };
-static const char dmenufont[]       = "firacode:size=10";
-static const char col_bg[]	    = "#282c34";
-static const char col_border[]      = "#343840";
-static const char col_bg_text[]     = "#abb2bf";
-static const char col_accent_text[] = "#282c34";
-static const char col_accent[]      = "#528bff";
+static const char *fonts[]          = { "firacode:size=12" };
+static const char dmenufont[]       = "firacode:size=12";
+static const char col_bg[]	    = "#fafafa";
+static const char col_border[]      = "#eff0f0";
+static const char col_bg_text[]     = "#8a9199";
+static const char col_accent_text[] = "#fafafa";
+static const char col_accent[]      = "#ff9940";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_bg_text, col_bg, col_border },
@@ -30,9 +30,6 @@ static const Rule rules[] = {
 	 */
 	/* class		instance	title	tags mask	is floating		monitor */
 	{ "Gimp",		NULL,		NULL,	0,		1,				-1 },
-	{ "Firefox",		NULL,		NULL,	1 << 8,		0,				-1 },
-	{ "QjackCtl",		NULL,		NULL,	1 << 7, 	0,				 0 },
-	{ "lsp-plugins",	NULL,		NULL,	1 << 7,		0,				 0 },
 	{ "discord",		NULL,		NULL,	1 << 7,		0,				 1 },
 };
 
@@ -66,46 +63,63 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bg, "-nf", col_bg_text, "-sb", col_accent, "-sf", col_accent_text, NULL };
+static const char *rofi[] = { "rofi", "-show", "run", NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *spotify[] = {"st", "-e", "start-spotify", NULL};
-static const char *qutebrowser[] = {"qutebrowser", NULL};
-static const char *editor[] = {"emacs", NULL};
+static const char *spotify[] = {"spotify", NULL};
+static const char *browser[] = {"firefox", NULL};
+static const char *steam[] = {"steam", NULL};
+static const char *editor[] = {"emacsclient", "-c", NULL};
+static const char *r2modman[] = {"r2modman", NULL};
+
+
+static const char *upvol[]   = { "amixer", "-c", "HD", "sset", "PCM", "playback", "volume", "-M", "2%+", NULL };
+static const char *downvol[] = { "amixer", "-c", "HD", "sset", "PCM", "playback", "volume", "-M", "2%-", NULL };
+static const char *mutevol[] = { "amixer", "-c", "HD", "sset", "PCM", "playback", "volume", "-M", "0", NULL };
+
+static const char *screenshot[] = { "maim", "~/Screenshots/$(date", "+%s).png", NULL };
 
 #include "shiftview.c"
+#define PrintScreen         0x0000ff61
+#include <X11/XF86keysym.h>
 static Key keys[] = {
-	/* modifier			key			function			argument					description */
+  /* modifier			key			function			argument					description */
+  { 0,                       XF86XK_AudioLowerVolume, spawn, {.v = downvol } },
+  { 0,                       XF86XK_AudioMute, spawn, {.v = mutevol } },
+  { 0,                       XF86XK_AudioRaiseVolume, spawn, {.v = upvol   } },
+  { 0,                       PrintScreen,             spawn, {.v = screenshot}},
+  { 0,				XK_Menu,	spawn,				{.v = rofi }},			/* dmenu */
 
-	{ 0,				XK_Menu,	spawn,				{.v = dmenucmd } },			/* dmenu */
+  LAUNCH(				XK_t,							termcmd)					/* terminal */
+  LAUNCH(				XK_s,							browser)				/* qutebrowser */
+  LAUNCH(				XK_r,							spotify)					/* spotify  */
+  LAUNCH(				XK_a,							steam)						/* editor  */
+  LAUNCH(                         XK_d,                                                   r2modman)
+  LAUNCH(           XK_g,                           editor)
+  
+  { MODKEY,			XK_n,		focusstack,			{.i = +1 } },				/* change stack focus */
+  { MODKEY,			XK_e,		focusstack,			{.i = -1 } },				/* change stack focus */
 
-	LAUNCH(				XK_t,							termcmd)					/* terminal */
-	LAUNCH(				XK_s,							qutebrowser)				/* qutebrowser */
-	LAUNCH(				XK_r,							spotify)					/* spotify  */ 
-	LAUNCH(				XK_a,							editor)						/* editor  */
+  { MODKEY|MODKEY2,	XK_m,		setmfact,			{.f = -0.05} },				/* change horizontal size of focused */
+  { MODKEY|MODKEY2,	XK_i,		setmfact,			{.f = +0.05} },				/* change horizontal size of focused */
+  { MODKEY,			XK_space,	zoom,				{0} },						/* swaps with largest */
 
-	{ MODKEY,			XK_n,		focusstack,			{.i = +1 } },				/* change stack focus */
-	{ MODKEY,			XK_e,		focusstack,			{.i = -1 } },				/* change stack focus */
+  { MODKEY,			XK_Menu,	killclient,			{0} },						/* kills window */
+  { MODKEY|MODKEY2,	XK_Return,	togglefloating,		{0} },						/* toggle window float / tile */
 
-	{ MODKEY|MODKEY2,	XK_m,		setmfact,			{.f = -0.05} },				/* change horizontal size of focused */
-	{ MODKEY|MODKEY2,	XK_i,		setmfact,			{.f = +0.05} },				/* change horizontal size of focused */
-	{ MODKEY,			XK_space,	zoom,				{0} },						/* swaps with largest */
+  { MODKEY,			XK_o,		focusmon,			{.i = +1 } },				/* change monitor focus */
+  { MODKEY|MODKEY2,	XK_o,		tagmon,				{.i = +1 } },				/* move to monitor */
+  { MODKEY,			XK_i,		shiftview,			{.i = +1} },				/* view next tab */
+  { MODKEY,			XK_m,		shiftview,			{.i = -1} },				/* view prev tab */
 
-	{ MODKEY,			XK_Menu,	killclient,			{0} },						/* kills window */
-	{ MODKEY|MODKEY2,	XK_Return,	togglefloating,		{0} },						/* toggle window float / tile */
-
-	{ MODKEY,			XK_o,		focusmon,			{.i = +1 } },				/* change monitor focus */
-	{ MODKEY|MODKEY2,	XK_o,		tagmon,				{.i = +1 } },				/* move to monitor */
-	{ MODKEY,			XK_i,		shiftview,			{.i = +1} },				/* view next tab */
-	{ MODKEY,			XK_m,		shiftview,			{.i = -1} },				/* view prev tab */
-
-	TAGKEYS(			XK_q,							0)							/* keybinds for tag 1 */
-	TAGKEYS(			XK_w,							1)							/* keybinds for tag 2 */
-	TAGKEYS(			XK_f,							2)							/* keybinds for tag 3 */
-	TAGKEYS(			XK_p,							3)							/* keybinds for tag 4 */
-	TAGKEYS(			XK_l,							4)							/* keybinds for tag 5 */
-	TAGKEYS(			XK_u,							5)							/* keybinds for tag 6 */
-	TAGKEYS(			XK_y,							6)							/* keybinds for tag 7 */
-	TAGKEYS(			XK_ooblique,					7)							/* keybinds for tag 8 */
-	{ ControlMask|ShiftMask,	XK_Escape,		quit,				{0} },						/* quit dwm */
+  TAGKEYS(			XK_q,							0)							/* keybinds for tag 1 */
+  TAGKEYS(			XK_w,							1)							/* keybinds for tag 2 */
+  TAGKEYS(			XK_f,							2)							/* keybinds for tag 3 */
+  TAGKEYS(			XK_p,							3)							/* keybinds for tag 4 */
+  TAGKEYS(			XK_l,							4)							/* keybinds for tag 5 */
+  TAGKEYS(			XK_u,							5)							/* keybinds for tag 6 */
+  TAGKEYS(			XK_y,							6)							/* keybinds for tag 7 */
+  TAGKEYS(			XK_semicolon,					7)							/* keybinds for tag 8 */
+  { ControlMask|ShiftMask,	XK_Escape,		quit,				{0} },						/* quit dwm */
 
 
  /* { MODKEY,			XK_b,		togglebar,			{0} },*/					/* ???? toggle task bar probably ???? */
