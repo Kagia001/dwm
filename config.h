82ba5ee9f09fbd47d3d1b29888192dae50b1a28b
karl.giese <karl@giese.no>
2022-06-19 19:26:39 +0200

    setup wo. colors

--- config.h
+++ config.h
@@ -2,12 +2,12 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const int gappx     = 5;                 /* gaps between windows */
+static const int gappx              = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "firacode:size=10" };
+static const char dmenufont[]       = "firacode:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -28,8 +28,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* { "Gimp",     NULL,       NULL,       0,            1,           -1 }, */
+	/* { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 }, */
+  { NULL, NULL, NULL, 0, 0, -1 }
 };
 
 /* layout(s) */
@@ -47,10 +48,16 @@ static const Layout layouts[] = {
 
 /* key definitions */
 #define MODKEY Mod1Mask
+#define MODKEY2 ShiftMask
+#define RKEY XK_Print
+
+#include <X11/XF86keysym.h>
+#include "shiftview.c"
+
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
-	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+	{ MODKEY|MODKEY2,               KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
@@ -59,46 +66,46 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *rofi[] = { "rofi", "-show", "run", NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *spotify[] = {"spotify", NULL};
+static const char *browser[] = {"firefox", NULL};
+static const char *editor[] = {"emacs", NULL};
+
+static const char *upvol[]   = { "amixer", "-c", "HD", "sset", "PCM", "playback", "volume", "-M", "2%+", NULL };
+static const char *downvol[] = { "amixer", "-c", "HD", "sset", "PCM", "playback", "volume", "-M", "2%-", NULL };
+static const char *mutevol[] = { "amixer", "-c", "HD", "sset", "PCM", "playback", "volume", "-M", "0", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,                       XK_minus,  setgaps,        {.i = -5 } },
-	{ MODKEY,                       XK_equal,  setgaps,        {.i = +5 } },
-	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+        { 0,              XF86XK_AudioLowerVolume, spawn,          {.v = downvol } },
+        { 0,                     XF86XK_AudioMute, spawn,          {.v = mutevol } },
+        { 0,              XF86XK_AudioRaiseVolume, spawn,          {.v = upvol } },
+	{ 0,                            RKEY,   spawn,          {.v = rofi } },
+	{ MODKEY,                       XK_t,      spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_s,      spawn,          {.v = browser } },
+	{ MODKEY,                       XK_r,      spawn,          {.v = spotify} },
+	{ MODKEY,                       XK_a,      spawn,          {.v = editor } },
+
+	{ MODKEY,                       XK_n,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_e,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_m,      shiftview,      {.i = -1 } },
+	{ MODKEY,                       XK_i,      shiftview,      {.i = +1 } },
+	{ MODKEY|MODKEY2,               XK_m,      setmfact,       {.f = -0.05} },
+	{ MODKEY|MODKEY2,               XK_i,      setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_space,  zoom,           {0} },
+	{ MODKEY,                       RKEY,   killclient,     {0} },
+	{ MODKEY,                       XK_1,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_2,      setlayout,      {.v = &layouts[2]} },
+	TAGKEYS(                        XK_q,                      0)
+	TAGKEYS(                        XK_w,                      1)
+	TAGKEYS(                        XK_f,                      2)
+	TAGKEYS(                        XK_p,                      3)
+	TAGKEYS(                        XK_l,                      4)
+	TAGKEYS(                        XK_u,                      5)
+	TAGKEYS(                        XK_y,                      6)
+	TAGKEYS(                        XK_semicolon,              7)
+	{ ControlMask|ShiftMask,        XK_Escape, quit,           {0} },
 };
 
 /* button definitions */
