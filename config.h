02396beccc47c56457fc09e38feb94d26c879b64
karl.giese <karl@giese.no>
2022-06-19 23:24:46 +0200

    remove bar

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const int gappx              = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
+static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "firacode:size=10" };
 static const char dmenufont[]       = "firacode:size=10";
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "Q", "W", "F", "P", "L", "U", "Y", ";"};
 
 static const Rule rules[] = {
 	/* xprop(1):
