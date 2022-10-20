64207f3f0487acddb22627103073c7b70197148f
karl.giese <karl@giese.no>
2022-10-20 21:38:21 +0200

    origin floating rule

--- config.h
+++ config.h
@@ -32,6 +32,7 @@ static const Rule rules[] = {
     /* class      instance    title       tags mask     isfloating   monitor */
     /* { "Gimp",     NULL,       NULL,       0,            1,           -1 }, */
     /* { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 }, */
+  { "origin.exe", NULL, NULL, 0, 1, -1},
     {NULL, NULL, NULL, 0, 0, -1}};
 
 static const char *uskeyboard[] = {"setxkbmap", "us", NULL};
